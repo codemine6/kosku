e0e2b313fcc238949c2a78344b5b3d4dca8900d2
mycodes33 <codemine6@gmail.com>
2021-02-07 13:31:11 +0700

    Update

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -5,13 +5,12 @@ import Button from 'components/Button/Button'
 
 export default function NotFound() {
     let history = useHistory()
-    console.log(history)
 
     return (
         <div className={styles.container}>
             <h1>404</h1>
             <h3>Nothing here..</h3>
-            <Button onClick={() => history.goBack()}>Go Back</Button>
+            <Button onClick={() => history.go(- (history.length -1))}>Go Home</Button>
         </div>
     )
 }
--- src/pages/Verification/Verification.js
+++ src/pages/Verification/Verification.js
@@ -4,9 +4,11 @@ import {useAuth, useDb} from 'services/Api'
 import styles from './Verification.module.css'
 
 import Button from 'components/Button/Button'
+import Loader from 'components/Loader/Loader'
 
 export default function Verification() {
     const [verified, setVerified] = useState(false)
+    const [loading, setLoading] = useState(true)
     let history = useHistory()
     let location = useLocation()
     let actionCode = new URLSearchParams(useLocation().search).get('oobCode')
@@ -16,33 +18,39 @@ export default function Verification() {
             useAuth.applyActionCode(actionCode).then(() => {
                 useDb.collection('users').doc(useAuth.currentUser.uid).set({emailVerified: true}, {merge: true})
                 setVerified(true)
+                setLoading(false)
             }).catch(() => {
                 history.replace('/404')
             })
-            setVerified(true)
         } else {
             useAuth.onAuthStateChanged(user => {
                 if (user.emailVerified && location.state) {
                     setVerified(true)
+                    setLoading(false)
                 } else if (user.emailVerified) {
                     history.replace('/404')
                 } else {
                     useAuth.currentUser.sendEmailVerification()
+                    setLoading(false)
                 }
             })
         }
     }, [actionCode, location, history])
 
     return (
-        <div className={styles.center}>
-            {verified ? <div>
-                <h3>Verifikasi Berhasil</h3>
-                <p>Akun berhasil diverifikasi. Silahkan klik lanjutkan untuk mulai menggunakan aplikasi.</p>
-                <Button onClick={() => history.replace('/')}>Lanjutkan</Button>
-            </div> : <div>
-                <h3>Verifikasi Akun</h3>
-                <p>Tautan verifikasi telah dikirim ke emailmu. Silahkan lakukan verifikasi untuk menyelesaikan pendaftaran.</p>
-            </div>}
+        <div className={styles.container}>
+            <div className={styles.content}>
+                {!loading && verified && <div>
+                    <h2>Verifikasi Berhasil</h2>
+                    <p>Akun berhasil diverifikasi. Silahkan klik lanjutkan untuk mulai menggunakan aplikasi.</p>
+                    <Button onClick={() => history.replace('/')}>Lanjutkan</Button>
+                </div>}
+                {!loading && !verified && <div>
+                    <h2>Verifikasi Email</h2>
+                    <p>Tautan verifikasi telah dikirim ke emailmu. Silahkan lakukan verifikasi untuk menyelesaikan pendaftaran.</p>
+                </div>}
+            </div>
+            {loading && <Loader/>}
         </div>
     )
 }
